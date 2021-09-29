c4b1b0b804f289b3d2938d5f6b3f3de92457af23
Esteban De La Fuente Rubio <esteban@delaf.cl>
2021-09-29 09:30:38 -0300

    Se agrega ejemplo autorización DTE de un contribuyente

---
+++ ejemplos/sii/dte.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+LibreDTE API Client
+Copyright (C) SASCO SpA (https://sasco.cl)
+
+Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+bajo los términos de la GNU Lesser General Public License (LGPL) publicada
+por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+o (a su elección) cualquier versión posterior de la misma.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN
+GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de APTITUD
+PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la GNU Lesser General
+Public License (LGPL) para obtener una información más detallada.
+
+Debería haber recibido una copia de la GNU Lesser General Public License
+(LGPL) junto a este programa. En caso contrario, consulte
+<http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+import os, sys
+app_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, app_dir)
+
+from libredte.api_client.sii.dte import Contribuyentes
+
+
+# datos del ejemplo
+RUT = ''
+
+# crear cliente de la API
+c = Contribuyentes()
+
+# CASO 1: autorización DTE
+print(c.autorizacion(RUT), end="\n\n")
---
+++ libredte/api_client/sii/dte.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+LibreDTE API Client
+Copyright (C) SASCO SpA (https://sasco.cl)
+
+Este programa es software libre: usted puede redistribuirlo y/o modificarlo
+bajo los términos de la GNU Lesser General Public License (LGPL) publicada
+por la Fundación para el Software Libre, ya sea la versión 3 de la Licencia,
+o (a su elección) cualquier versión posterior de la misma.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN
+GARANTÍA ALGUNA; ni siquiera la garantía implícita MERCANTIL o de APTITUD
+PARA UN PROPÓSITO DETERMINADO. Consulte los detalles de la GNU Lesser General
+Public License (LGPL) para obtener una información más detallada.
+
+Debería haber recibido una copia de la GNU Lesser General Public License
+(LGPL) junto a este programa. En caso contrario, consulte
+<http://www.gnu.org/licenses/lgpl.html>.
+"""
+
+from ..base import LibreDTEApiBase
+
+
+class Contribuyentes(LibreDTEApiBase):
+
+    def autorizacion(self, rut, certificacion = False):
+        r = self.client.get('/sii/dte/contribuyentes/autorizado/%(rut)s?certificacion=%(certificacion)d' % {
+            'rut': str(rut),
+            'certificacion': 1 if certificacion else 0,
+        })
+        return r.json()
